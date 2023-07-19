6ece59b558c0b75e291246548ab355dbbe863c75
Soma53 <hgsn.sum@gmail.com>
2023-07-19 11:37:04 +0900

    310d,e

--- CPLUS/abc300/310/C.cpp
+++ CPLUS/abc300/310/C.cpp
@@ -1,6 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    
+ 
+string reverse_string(const string& s) {
+    string reversed = s;
+    reverse(reversed.begin(), reversed.end());
+    return reversed;
+}
+ 
+int main() {
+    int N;
+    cin >> N;
+    set<pair<string, string>> rods;
+    for (int i = 0; i < N; i++) {
+        string S;
+        cin >> S;
+        string reversed_S = reverse_string(S);
+        if (S > reversed_S) swap(S, reversed_S);
+        rods.insert({S, reversed_S});
+    }
+    cout << rods.size() << endl;
+    return 0;
 }
